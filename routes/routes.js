77510aa2a859add07057f159501e3001fa5ba18e
Sharon Funke <sharon.funke@nzz.ch>
2017-12-23 18:50:12 +0100

    WIP: Add fixture data and endpoints accordingly

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,8 @@
-const htmlStatic = require('./rendering-info/html-static.js');
-const stylesheet = require('./stylesheet.js');
-const schema = require('./schema.js');
-const locales = require('./locales.js');
-const migration = require('./migration.js');
-
 module.exports = [
-	htmlStatic,
-	stylesheet,
-	schema,
-  locales,
-  migration
+	require('./rendering-info/html-static.js'),
+	require('./stylesheet.js'),
+	require('./schema.js'),
+  require('./locales.js'),
+  require('./fixtures/data.js'),
+  require('./migration.js')
 ]
