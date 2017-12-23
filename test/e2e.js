b73e26280560e7ae661fcdf5b36bf3db1fa1bbb9
Sharon Funke <sharon.funke@nzz.ch>
2017-12-23 18:49:45 +0100

    Fix tests

--- test/e2e.js
+++ test/e2e.js
@@ -1,62 +1,49 @@
 'use strict';
 
-const Hoek = require('hoek');
 const expect = require('chai').expect;
 const server = require('../server.js');
 const plugins = require('../server-plugins.js');
 const routes = require('../routes/routes.js');
 
-server.register(plugins, err => {
-  Hoek.assert(!err, err);
-
+async function start() {
+  await server.register(plugins);
   server.route(routes);
+  await server.start();
 
-  server.start(err => {
-    Hoek.assert(!err, err);
-  })
+  // some basic API tests
+  describe('Q required API', () => {
 
-});
+    it('should return 200 for /schema.json', async () => {
+      const response = await server.inject('/schema.json')
+      expect(response.statusCode).to.be.equal(200);
+    })
 
-// some basic API tests
-describe('Q required API', () => {
+    it('should return 200 for /stylesheet/default.123.css', async () => {
+      const response = await server.inject('/stylesheet/default.123.css') 
+      expect(response.statusCode).to.be.equal(200);
+    })
 
-  it('should return 200 for /schema.json', function(done) {
-    server.inject('/schema.json', (res) => {
-      expect(res.statusCode).to.be.equal(200);
-      done();
-    });
-  })
+    it('should return 404 for inexistent stylesheet', async () => {
+      const response = await server.inject('/stylesheet/inexisting.123.css');
+      expect(response.statusCode).to.be.equal(404);
+    })
 
-  it('should return 200 for /stylesheet/default.123.css', function(done) {
-    server.inject('/stylesheet/default.123.css', (res) => {
-      expect(res.statusCode).to.be.equal(200);
-      done()
-    });
-  })
+  });
 
-  it('should return 404 for inexistent stylesheet', function(done) {
-    server.inject('/stylesheet/inexisting.123.css', (res) => {
-      expect(res.statusCode).to.be.equal(404);
-      done()
-    });
-  })
+  const mockData = JSON.parse(JSON.stringify(require('./resources/mock-data.js')));
 
-});
+  describe('rendering-info endpoints', () => {
 
-const mockData = JSON.parse(JSON.stringify(require('./resources/mock-data.js')));
+    it('should return 200 for /rendering-info/html-static', async () => {
+      const request = {
+        method: 'POST',
+        url: '/rendering-info/html-static',
+        payload: JSON.stringify({ item: mockData })
+      };
+      const response = await server.inject(request);
+      expect(response.statusCode).to.be.equal(200);
+    })
+  });
+}
 
-describe('rendering-info endpoints', () => {
-
-  it('should return 200 for /rendering-info/html-static', function(done) {
-    const request = {
-      method: 'POST',
-      url: '/rendering-info/html-static',
-      payload: JSON.stringify({ item: mockData })
-    };
-    server.inject(request, (res) => {
-      expect(res.statusCode).to.be.equal(200);
-      done();
-    });
-  })
-
-});
+start();
