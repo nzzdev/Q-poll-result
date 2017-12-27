32fc7ec3ea0609d011e0cd2b979b0a37a707c7bc
Sharon Funke <sharon.funke@nzz.ch>
2017-12-27 12:13:26 +0100

    Remove tabs

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 module.exports = [
-	require('./rendering-info/html-static.js'),
-	require('./stylesheet.js'),
-	require('./schema.js'),
+  require('./rendering-info/html-static.js'),
+  require('./stylesheet.js'),
+  require('./schema.js'),
   require('./locales.js'),
   require('./fixtures/data.js'),
   require('./migration.js')
