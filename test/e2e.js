960098b37270b14c359b5bdeb0cc0c7518096705
fuenkchen <sharon.funke@voodoo-arts.net>
2018-01-09 17:29:41 +0100

    Release 1.4.0 (#11)
    * Hapi update & add fixture data (#10)
    
    * New release version 1.4.0

--- test/e2e.js
+++ test/e2e.js
@@ -1,62 +1,89 @@
 'use strict';
 
-const Hoek = require('hoek');
 const expect = require('chai').expect;
 const server = require('../server.js');
 const plugins = require('../server-plugins.js');
 const routes = require('../routes/routes.js');
 
-server.register(plugins, err => {
-  Hoek.assert(!err, err);
-
+async function start() {
+  await server.register(plugins);
   server.route(routes);
+  await server.start();
 
-  server.start(err => {
-    Hoek.assert(!err, err);
-  })
-
-});
-
-// some basic API tests
-describe('Q required API', () => {
-
-  it('should return 200 for /schema.json', function(done) {
-    server.inject('/schema.json', (res) => {
-      expect(res.statusCode).to.be.equal(200);
-      done();
+  describe('basic routes', () => {
+    it('starts the server', () => {
+      expect(server.info.port).to.be.equal(3000);
     });
-  })
-
-  it('should return 200 for /stylesheet/default.123.css', function(done) {
-    server.inject('/stylesheet/default.123.css', (res) => {
-      expect(res.statusCode).to.be.equal(200);
-      done()
+  
+    it('is healthy', async () => {
+      const response = await server.inject('/health');
+      expect(response.payload).to.be.equal('ok');
     });
-  })
+  });
 
-  it('should return 404 for inexistent stylesheet', function(done) {
-    server.inject('/stylesheet/inexisting.123.css', (res) => {
-      expect(res.statusCode).to.be.equal(404);
-      done()
+  describe('schema endpoint', () => {
+
+    it('returns 200 for /schema.json', async () => {
+      const response = await server.inject('/schema.json');
+      expect(response.statusCode).to.be.equal(200);
     });
-  })
+  
+  });
 
-});
-
-const mockData = JSON.parse(JSON.stringify(require('./resources/mock-data.js')));
-
-describe('rendering-info endpoints', () => {
-
-  it('should return 200 for /rendering-info/html-static', function(done) {
-    const request = {
-      method: 'POST',
-      url: '/rendering-info/html-static',
-      payload: JSON.stringify({ item: mockData })
-    };
-    server.inject(request, (res) => {
-      expect(res.statusCode).to.be.equal(200);
-      done();
+  describe('stylesheet endpoint', () => {
+    
+    it('returns 200 for /stylesheet/default.123.css', async () => {
+      const response = await server.inject('/stylesheet/default.123.css')
+      expect(response.statusCode).to.be.equal(200);
     });
-  })
+  
+    it('returns 404 for inexistent stylesheet', async () => {
+      const response = await server.inject('/stylesheet/inexisting.123.css')
+      expect(response.statusCode).to.be.equal(404);
+    });
+  
+  });
 
-});
+  const mockData = require('../resources/fixtures/data/mixed-3-5-sorted.json');
+
+  describe('rendering-info endpoint', () => {
+
+    it('should return 200 for /rendering-info/html-static', async () => {
+      const request = {
+        method: 'POST',
+        url: '/rendering-info/html-static',
+        payload: { 
+          item: mockData 
+        }
+      };
+      const response = await server.inject(request);
+      expect(response.statusCode).to.be.equal(200);
+    })
+  });
+
+  describe('migration endpoint', () => {
+    
+    it('returns 304 for /migration', async () => {
+      const request = {
+        method: 'POST',
+        url: '/migration',
+        payload: { 
+          item: mockData
+        }
+      };
+      const response = await server.inject(request);
+      expect(response.statusCode).to.be.equal(304);
+    });
+  
+  });
+
+  describe('fixture data endpoint', () => {
+    it('returns 4 fixture data items for /fixtures/data', async () => {
+      const response = await server.inject('/fixtures/data');
+      expect(response.statusCode).to.be.equal(200);
+      expect(response.result.length).to.be.equal(4);
+    })
+  });
+}
+
+start();
