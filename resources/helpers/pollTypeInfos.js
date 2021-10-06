69605c3457b3ec1bbd14fa07a4bfaa96ad852d5a
Michel Grautstueck <michel.grautstueck@nzz.ch>
2021-10-06 11:18:47 +0200

    Code formatting

--- resources/helpers/pollTypeInfos.js
+++ resources/helpers/pollTypeInfos.js
@@ -4,47 +4,47 @@ module.exports = {
       {
         name: "certain-yes",
         label: "bestimmt dafür",
-        cssClass: "s-viz-color-diverging-one-5-5"
+        cssClass: "s-viz-color-diverging-one-5-5",
       },
       {
         name: "rather-yes",
         label: "eher dafür",
-        cssClass: "s-viz-color-diverging-one-5-4"
+        cssClass: "s-viz-color-diverging-one-5-4",
       },
       {
         name: "undecided",
         label: "weiss nicht",
-        cssClass: "s-viz-color-diverging-one-5-3"
+        cssClass: "s-viz-color-diverging-one-5-3",
       },
       {
         name: "rather-no",
         label: "eher dagegen",
-        cssClass: "s-viz-color-diverging-one-5-2"
+        cssClass: "s-viz-color-diverging-one-5-2",
       },
       {
         name: "certain-no",
         label: "bestimmt dagegen",
-        cssClass: "s-viz-color-diverging-one-5-1"
-      }
-    ]
+        cssClass: "s-viz-color-diverging-one-5-1",
+      },
+    ],
   },
   preVoteNoToYes3: {
     answers: [
       {
         name: "yes",
         label: "bestimmt/eher dafür",
-        cssClass: "s-viz-color-diverging-one-3-3"
+        cssClass: "s-viz-color-diverging-one-3-3",
       },
       {
         name: "undecided",
         label: "weiss nicht",
-        cssClass: "s-viz-color-diverging-one-3-2"
+        cssClass: "s-viz-color-diverging-one-3-2",
       },
       {
         name: "no",
         label: "bestimmt/eher dagegen",
-        cssClass: "s-viz-color-diverging-one-3-1"
-      }
-    ]
-  }
+        cssClass: "s-viz-color-diverging-one-3-1",
+      },
+    ],
+  },
 };
