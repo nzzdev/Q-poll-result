3c082975d24d48ad65245d1a6305f5d994e88ee0
Michel Grautstueck <michel.grautstueck@nzz.ch>
2021-10-06 14:07:01 +0200

    Code formatting

--- routes/stylesheet.js
+++ routes/stylesheet.js
@@ -6,13 +6,13 @@ module.exports = {
   options: {
     cors: true,
     files: {
-      relativeTo: path.join(__dirname, "/../styles/")
-    }
+      relativeTo: path.join(__dirname, "/../styles/"),
+    },
   },
-  handler: function(request, h) {
+  handler: function (request, h) {
     return h
       .file(`${request.params.filename}.${request.params.extension}`)
       .type("text/css")
       .header("cache-control", `max-age=${60 * 60 * 24 * 365}, immutable`); // 1 year
-  }
+  },
 };
