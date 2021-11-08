ad52ad4b7b67889dd1242148e3df60f315a4894e
Michel Grautstueck <michel.grautstueck@nzz.ch>
2021-11-08 10:15:48 +0100

    Separate Sass from svelte files

---
+++ views/components/Legend/Legend.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let item;
+</script>
+
+<ul class="q-poll-result-legend s-font-note-s">
+  {#each item.answers as answer}
+    <li class="q-poll-result-legend-item {answer.cssClass}">
+      <span class="s-font-note-s s-font-note-s--light">
+        {answer.label}
+      </span>
+    </li>
+  {/each}
+</ul>
